31b08498003501f580670545d17b4a023c22e5d7
twice 🐝 <_@twice.lol>
2022-05-24 18:45:43 +1000

    Implement as basic userscript

--- lexic.js
+++ lexic.js
@@ -1,23 +1,43 @@
-export const TAG = "__lexic_emphasis"
+const TAG = "__lexic_emphasis"
 
-const fraction = 3 / 4
-const round = Math.floor
+const BASE_WEIGHT = 400
+const TAGGED_WEIGHT = 600
+const TAGGED_FRACTION = 3 / 4
+const ROUNDING_FN = Math.floor
 
 /**
  * The number of characters to emphasise for the given length input string.
- * @param {number} len
+ * @param {number} wordlen
  */
-function n_emphasise(len) {
-  return len <= 3
-    ? len - 1
-    : round(len * fraction)
+function k_emphasise(wordlen) {
+  return wordlen <= 3
+    ? wordlen - 1
+    : ROUNDING_FN(wordlen * TAGGED_FRACTION)
 }
 
 /**
  * Add an emphasis tag to the given string (expected to correspond to a word).
- * @param {string} str
+ * @param {string} word
  */
-export function emphasise(str) {
-  const n = n_emphasise(str.length)
-  return `<span class="${TAG}">${str.slice(0, n)}</span>${str.slice(n)}`
+function emphasise(word) {
+  const k = k_emphasise(word.length)
+  return `<span class="${TAG}">${word.slice(0, k)}</span>${word.slice(k)}`
 }
+
+document.querySelectorAll("p, ol, ul").forEach(node => {
+  // TODO ignore if a child of certain nodes: e.g. time, code, nav, etc.
+  node.innerHTML = node.innerHTML.replaceAll(
+    /(?<!<[^>]*)\w+/g, // ignore html tags themselves
+    word => emphasise(word)
+  )
+})
+
+const style = document.createElement("style")
+document.head.appendChild(style)
+style.type = "text/css"
+
+// TODO detect and use the existing font weight
+style.appendChild(document.createTextNode(`
+  .${TAG} { font-weight: ${TAGGED_WEIGHT}; }
+  p { font-weight: ${BASE_WEIGHT}; }
+`))
