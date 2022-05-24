af389a473363a3523752c0383766c11ed55474e5
twice 🐝 <_@twice.lol>
2022-05-24 18:45:43 +1000

    Apply to document on ctrl-backquote

--- lexic.js
+++ lexic.js
@@ -1,18 +1,23 @@
+// ==UserScript==
+// @name        lexic
+// @author      twice 🐝
+// @namespace   https://github.com/comfytwice/lexic
+// @match       *://*/*
+// ==/UserScript==
+
 const TAG = "__lexic_emphasis"
 
-const BASE_WEIGHT = 400
-const TAGGED_WEIGHT = 600
 const TAGGED_FRACTION = 3 / 4
 const ROUNDING_FN = Math.floor
 
 /**
- * The number of characters to emphasise for the given length input string.
- * @param {number} wordlen
+ * The number of characters to emphasise for a given length input word.
+ * @param {number} length
  */
-function k_emphasise(wordlen) {
-  return wordlen <= 3
-    ? wordlen - 1
-    : ROUNDING_FN(wordlen * TAGGED_FRACTION)
+function k_emphasise(length) {
+  return length <= 3
+    ? length - 1
+    : ROUNDING_FN(length * TAGGED_FRACTION)
 }
 
 /**
@@ -24,20 +29,25 @@ function emphasise(word) {
   return `<span class="${TAG}">${word.slice(0, k)}</span>${word.slice(k)}`
 }
 
-document.querySelectorAll("p, ol, ul").forEach(node => {
-  // TODO ignore if a child of certain nodes: e.g. time, code, nav, etc.
-  node.innerHTML = node.innerHTML.replaceAll(
-    /(?<!<[^>]*)\w+/g, // ignore html tags themselves
-    word => emphasise(word)
-  )
-})
-
 const style = document.createElement("style")
 document.head.appendChild(style)
 style.type = "text/css"
-
-// TODO detect and use the existing font weight
 style.appendChild(document.createTextNode(`
-  .${TAG} { font-weight: ${TAGGED_WEIGHT}; }
-  p { font-weight: ${BASE_WEIGHT}; }
+  .${TAG} { font-weight: bold; }
 `))
+
+function applyToDocument() {
+  document.querySelectorAll("p, ol, ul, span").forEach(node => {
+    // TODO ignore if a child of certain nodes: e.g. time, code, nav, etc.
+    node.innerHTML = node.innerHTML.replaceAll(
+      /(?<!<[^>]*)\w+/g, // ignore html tags themselves
+      word => emphasise(word)
+    )
+  })
+}
+
+document.addEventListener("keydown", function ({ ctrlKey, which }) {
+  if (ctrlKey && which === 192) {
+    applyToDocument()
+  }
+})
